a5c6c63e054408b1e4b87f63f8e8848247ffc404
kimdaehyun <zbeld123@gmail.com>
2021-06-27 17:31:16 +0900

    adapter pattern

---
+++ src/adapter/ExistingFeatures.java
@@ -0,0 +1,16 @@
+package adapter;
+
+// ==========================
+// 사용하고자 하는 기존에 있는 알고리즘
+// ==========================
+
+public class ExistingFeatures {
+
+    public double doubleNumber(double number) {
+        return number * 2;
+    }
+
+    public double halfNumber(double number) {
+        return number / 2;
+    }
+}
